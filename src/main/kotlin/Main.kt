ad3b275e7dec7dff9f2fd10b01461c21c79c2753
inchang <3721cha21.11@gmail.com>
2022-08-16 17:52:10 +0900

    Feat : add Launch and Async example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,20 +5,6 @@ import java.time.temporal.ChronoUnit
 fun now()=ZonedDateTime.now().toLocalTime().truncatedTo(ChronoUnit.MILLIS)
 fun log(msg: String) = println("${now()}:${Thread.currentThread()}: ${msg}")
 
-fun launchInGlobalScope(){
-    GlobalScope.launch {
-        log("coroutine started")
-    }
-}
-
-fun main1() {
-    log("main started")
-    launchInGlobalScope()
-    log("launchInGlobalScope() executed")
-    Thread.sleep(5000L)
-    log("main() terminated")
-}
-
 fun main2() = runBlocking<Unit> {
     launch {   // 인자를 안 넘겨주면 상위 코루틴의 Context 를 가짐
         println("main runBlocking       : 나는 ${Thread.currentThread().name} 에서 돌아")
@@ -34,22 +20,6 @@ fun main2() = runBlocking<Unit> {
     }
 }
 
-
-fun sumAll(){
-    runBlocking{
-        val d1 = async{delay(1000L); 1}
-        log("after aync(d1)")
-        val d2 = async{delay(1000L); 2}
-        log("after aync(d2)")
-        val d3 = async{delay(1000L); 3}
-        log("after aync(d3)")
-
-        log("1+2+3=${d1.await() + d2.await() + d3.await()}")
-        log("after await all & add")
-    }
-}
-
-//B
 fun countdown() = sequence {
     var n = 10
     while(n > 0){
@@ -64,7 +34,36 @@ fun example1(){
     }
 }
 
+fun launchInGlobalScope(){
+    GlobalScope.launch {
+        log("coroutine started")
+    }
+}
+fun exampleLaunch(){
+    log("main started")
+    launchInGlobalScope()
+    log("launchInGlobalScope() executed")
+    Thread.sleep(5000L)
+    log("main() terminated")
+}
+
+fun exampleAsync(){
+    runBlocking{
+        val d1 = async{delay(1000L); 1}
+        log("after aync(d1)")
+        val d2 = async{delay(1000L); 2}
+        log("after aync(d2)")
+        val d3 = async{delay(1000L); 3}
+        log("after aync(d3)")
+
+        log("1+2+3=${d1.await() + d2.await() + d3.await()}")
+        log("after await all & add")
+    }
+}
+
 fun main(){
-    example1()
+//    example1()
+//    exampleLaunch()
+    exampleAsync()
 
 }
