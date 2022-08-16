2d10e1e138114e0cac98a38dec21aa9c9e02fe8f
inchang <3721cha21.11@gmail.com>
2022-08-16 17:54:13 +0900

    Feat : add dispatcher example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,21 +5,6 @@ import java.time.temporal.ChronoUnit
 fun now()=ZonedDateTime.now().toLocalTime().truncatedTo(ChronoUnit.MILLIS)
 fun log(msg: String) = println("${now()}:${Thread.currentThread()}: ${msg}")
 
-fun main2() = runBlocking<Unit> {
-    launch {   // 인자를 안 넘겨주면 상위 코루틴의 Context 를 가짐
-        println("main runBlocking       : 나는 ${Thread.currentThread().name} 에서 돌아")
-    }
-    launch(Dispatchers.Unconfined) {  // 별도로 지정을 안 해줌
-        println("Unconfined             : 나는 ${Thread.currentThread().name} 에서 돌아")
-    }
-    launch(Dispatchers.Default) {  // DefaultDispatchers 등록
-        println("Default                : 나는 ${Thread.currentThread().name} 에서 돌아")
-    }
-    launch(newSingleThreadContext("H43RO_Thread")) {  // 새로운 쓰레드 생성
-        println("newSingleThreadContext : 나는 ${Thread.currentThread().name} 에서 돌아")
-    }
-}
-
 fun countdown() = sequence {
     var n = 10
     while(n > 0){
@@ -61,9 +46,26 @@ fun exampleAsync(){
     }
 }
 
+fun exampleDispatcher() {
+    runBlocking<Unit> {
+        launch {   // 인자를 안 넘겨주면 상위 코루틴의 Context 를 가짐
+            println("main runBlocking       : 나는 ${Thread.currentThread().name} 에서 돌아")
+        }
+        launch(Dispatchers.Unconfined) {  // 별도로 지정을 안 해줌
+            println("Unconfined             : 나는 ${Thread.currentThread().name} 에서 돌아")
+        }
+        launch(Dispatchers.Default) {  // DefaultDispatchers 등록
+            println("Default                : 나는 ${Thread.currentThread().name} 에서 돌아")
+        }
+        launch(newSingleThreadContext("H43RO_Thread")) {  // 새로운 쓰레드 생성
+            println("newSingleThreadContext : 나는 ${Thread.currentThread().name} 에서 돌아")
+        }
+    }
+}
+
 fun main(){
 //    example1()
+//    exampleDispatcher()
 //    exampleLaunch()
-    exampleAsync()
-
+//    exampleAsync()
 }
